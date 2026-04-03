241864f06c5ec989adf7b482f19922174df26c95
Timmy Chen <timmychen748@gmail.com>
2026-04-03 18:12:31 -0300

    fix(api): arquivo de migração para tabela de usuários consertada

--- packages/api/migrations/env.py
+++ packages/api/migrations/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from api.database import table_registry
 from api.settings import Settings
+from api.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
